404678bf2133dd8822dba742674407a627b34882
Jayant Kumar Gupta <jayant0gupta6@gmail.com>
2025-11-16 07:52:06 +0000

    practiced linked list portion 3

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ class Node:
 
 node1 = Node(1)
 node2 = Node(3)
-node3 = Node(5)
+node3 = Node(9)
 node4 = Node(7)
-node5 = Node(9)
+node5 = Node(5)
 
 node1.next = node2
 node2.next = node3
 node3.next = node4
 node4.next = node5
+node5.next = node1
 
 def findGreatest(head):
     greatest = head.data
@@ -56,6 +57,29 @@ def insertnode(head,nodetoinsert,position):
     currentNode.next = newnode
     return head
 
+def detect_loop(head):
+    fast = head
+    slow = head
+    while fast and fast.next:
+        if fast.next is slow:
+            return True
+        fast = fast.next.next
+        slow = slow.next
+    return False
+
+def reverse_ll(head):
+    previous = None
+    currentNode = head
+    while currentNode:
+        nxt = currentNode.next
+        currentNode.next = previous
+        previous = currentNode
+        currentNode = nxt
+
+    return previous
+        
+
+
 
 # Find greatest node among Linked list
 # print("The greatest number among the linked list is: ",findGreatest(node1))
@@ -68,5 +92,15 @@ def insertnode(head,nodetoinsert,position):
 # print(travel(node1))
 
 # Insert Node
-# node1 = insertnode(node1,Node(2),6)
+# node1 = insertnode(node1,Node(6),3)
+# print(travel(node1))
+
+# Detect cycle in Linked List
+# if detect_loop(node1):
+#     print("Linked list is cyclomatic")
+# else:
+#     print("Linked list is  not cyclomatic")
+
+# Reverse Linked List
+# node1 = reverse_ll(node1)
 # print(travel(node1))
