f856dba1ce2e917d8ac2364992a77b9c4c2ba3ed
Jayant Kumar Gupta <jayant0gupta6@gmail.com>
2026-01-06 19:15:29 +0000

    created a python program file which determines the amount of rainwater trapped in histogram.

--- rainwater_trapping.py
+++ rainwater_trapping.py
@@ -1,1 +1,22 @@
+def trapping_rainwater(array):
+    left = 0
+    max_left_value = array[left]
+    right = len(array)-1
+    max_right_value = array[right]
+    total = 0
+    while left != right:
+        if max_left_value <= max_right_value:
+            left+=1
+            max_left_value = max(max_left_value,array[left])
+            water = max_left_value-array[left]
+            total += water
+            
+        if max_right_value < max_left_value:
+            right-=1
+            max_right_value = max(max_right_value,array[right])
+            water = max_right_value-array[right]
+            total+=water
+            
+    return total
 
+print(trapping_rainwater([0,1,0,2,1,0,1,3,2,1,2,1]))
